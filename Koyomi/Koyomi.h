b3b898583394891d179aa1cd78d7df776628ec4e
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-10-28 14:22:53 +0900

    add header file

---
+++ Koyomi/Koyomi.h
@@ -0,0 +1,19 @@
+//
+//  Koyomi.h
+//  Koyomi
+//
+//  Created by Shohei Yokoyama on 2016/10/28.
+//  Copyright © 2016年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Koyomi.
+FOUNDATION_EXPORT double KoyomiVersionNumber;
+
+//! Project version string for Koyomi.
+FOUNDATION_EXPORT const unsigned char KoyomiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Koyomi/PublicHeader.h>
+
+
